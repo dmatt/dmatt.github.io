c00111d29ea494b8f14c31203e23492857f22c2f
dmatt <dmatt@disqus.com>
2021-07-01 11:45:00 -0700

    Add gravitec to test page

---
+++ push-worker.js
@@ -0,0 +1,6 @@
+self[`appKey`] = `1f9e37f051b183b447a64a0479db5e62`;
+self[`hostUrl`] = `https://cdn.gravitec.net/sw`;
+self.importScripts(`${self[`hostUrl`]}/worker.js`);
+// uncomment and set path to your service worker
+// if you have one with precaching functionality (has oninstall, onactivate event listeners)
+// self.importScripts('path-to-your-sw-with-precaching')
